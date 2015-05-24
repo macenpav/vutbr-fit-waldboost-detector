664fe2a8d09d4b574bc0168bfbebc7a30c73a7f5
Pavel Macenauer <pavel.macenauer@fotoaparat.cz>
2015-05-24 02:25:12 +0200

    another day, another backu

--- tests/DEBUG_30.bat
+++ tests/DEBUG_30.bat
@@ -1,3 +1,3 @@
 cd ../bin
-WaldboostDetector_30.exe -iv 1920x1080.mp4 -vd -bs 16 -v -ms -dm aglobal -t -lf 200 >> "../tests/debug_log.txt"
+WaldboostDetector_30.exe -iv 720x480.mp4 -vd -bs 16 -v -ms -dm aglobal -t -lf 200 >> "../tests/debug_log.txt"
 pause
